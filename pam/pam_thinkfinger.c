b99829a17fd6cb8f72acc4138c7f288f70a4cad6
thoenig <thoenig@90c7dc4f-7820-0410-8e86-a0f4997c6083>
2007-02-04 15:28:20 +0000

    Add support for pam_syslog, new error handling, always kill pam_prompt thread on exit of thinkfinger thread.

--- pam/pam_thinkfinger.c
+++ pam/pam_thinkfinger.c
@@ -23,11 +23,13 @@
 
 #include <config.h>
 
+#include <libthinkfinger.h>
+
 #include <stdio.h>
 #include <termios.h>
 #include <unistd.h>
 #include <pthread.h>
-#include <libthinkfinger.h>
+#include <syslog.h>
 #include <security/pam_modules.h>
 #ifdef HAVE_OLD_PAM
 #include "pam_thinkfinger-compat.h"
@@ -38,7 +40,6 @@
 #define MAX_PATH    256
 #define SWIPE_RETRY 3
 
-
 #define PAM_SM_AUTH
 
 struct pam_thinkfinger_s {
@@ -48,6 +49,7 @@ struct pam_thinkfinger_s {
 	pthread_t t_thinkfinger;
 	pthread_mutex_t retval_mutex;
 	int retval;
+	int isatty;
 	pam_handle_t *pamh;
 };
 
@@ -69,47 +71,59 @@ out:
 	return retval;
 }
 
-static int pam_thinkfinger_verify (const struct pam_thinkfinger_s *pam_thinkfinger)
+static libthinkfinger_state pam_thinkfinger_verify (const struct pam_thinkfinger_s *pam_thinkfinger)
 {
-	int tf_state;
-	int retval = 0;
+	libthinkfinger_state tf_state = TF_STATE_VERIFY_FAILED;
 	char bir_file[MAX_PATH];
 
 	snprintf (bir_file, MAX_PATH, "%s/%s.bir", PAM_BIRDIR, pam_thinkfinger->user);
 
 	if (pam_thinkfinger->tf == NULL)
-		return 1;
+		goto out;
 
 	libthinkfinger_set_file (pam_thinkfinger->tf, bir_file);
-
 	tf_state = libthinkfinger_verify (pam_thinkfinger->tf);
-	if (tf_state == TF_RESULT_VERIFY_SUCCESS) {
-		retval = 0;
-	} else if ((tf_state == TF_RESULT_VERIFY_FAILED) ||
-		   (tf_state == TF_RESULT_COMM_FAILED)) {
-		retval = 1;
-	}
 
-	return retval;
+out:
+	return tf_state;
 }
 
 static void thinkfinger_thread (void *data)
 {
 	struct pam_thinkfinger_s *pam_thinkfinger = data;
-	int i = 0;
+	libthinkfinger_state tf_state;
+	int retry = 0;
+	_Bool pending = true;
 
-	for (i = 0; i < SWIPE_RETRY; i++) {
-		if (!pam_thinkfinger_verify (pam_thinkfinger)) {
+	while (++retry <= SWIPE_RETRY && pending) {
+		tf_state = pam_thinkfinger_verify (pam_thinkfinger);
+		if (tf_state == TF_RESULT_VERIFY_SUCCESS) {
 			pthread_mutex_lock (&pam_thinkfinger->retval_mutex);
 			pam_thinkfinger->retval = PAM_SUCCESS;
 			pthread_mutex_unlock (&pam_thinkfinger->retval_mutex);
-			pthread_cancel (pam_thinkfinger->t_pam_prompt);
-			fprintf(stdout, "\n");
-			goto out;
+			pam_syslog (pam_thinkfinger->pamh, LOG_NOTICE,
+				    "%s authenticated (biometric identification record matched)",
+				    pam_thinkfinger->user);
+			pending = false;
+		} else if (tf_state == TF_RESULT_VERIFY_FAILED) {
+			pam_syslog (pam_thinkfinger->pamh, LOG_NOTICE,
+				    "%s verification failed (attempt %i/%i)",
+				    pam_thinkfinger->user, retry, SWIPE_RETRY);
+		} else {
+			pthread_mutex_lock (&pam_thinkfinger->retval_mutex);
+			pam_thinkfinger->retval = PAM_AUTH_ERR;
+			pthread_mutex_unlock (&pam_thinkfinger->retval_mutex);
+			pam_syslog (pam_thinkfinger->pamh, LOG_NOTICE,
+				    "%s verication failed (pam_thinkfinger error 0x%x)",
+				    pam_thinkfinger->user, tf_state);
+			pending = false;
 		}
+
 	}
 
-out:
+	if (pam_thinkfinger->isatty)
+		fprintf(stdout, "\n");
+	pthread_cancel (pam_thinkfinger->t_pam_prompt);
 	pthread_exit (NULL);
 }
 
@@ -128,15 +142,48 @@ static void pam_prompt_thread (void *data)
 	pthread_exit (NULL);
 }
 
+static void handle_error (pam_handle_t *pamh, libthinkfinger_init_status init_status)
+{
+	const char *msg;
+
+	switch (init_status) {
+	case TF_INIT_NO_MEMORY:
+		msg = "Not enough memory.";
+		break;
+	case TF_INIT_USB_DEVICE_NOT_FOUND:
+		msg = "USB device not found.";
+		break;
+	case TF_INIT_USB_OPEN_FAILED:
+		msg = "Could not open USB device.";
+		break;
+	case TF_INIT_USB_CLAIM_FAILED:
+		msg = "Could not claim USB device.";
+		break;
+	case TF_INIT_USB_HELLO_FAILED:
+		msg = "Sending HELLO failed.";
+		break;
+	case TF_INIT_UNDEFINED:
+		msg = "Undefined error.";
+		break;
+	default:
+		msg = "Unknown error.";
+	}	
+
+	pam_syslog (pamh, LOG_ERR, msg);
+
+	return;
+}
+
 PAM_EXTERN
 int pam_sm_authenticate (pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
 	struct pam_thinkfinger_s pam_thinkfinger;
 	struct termios term_attr;
-	_Bool tty = false;
+	libthinkfinger_init_status init_status;
+	int tty = 0;
 	
-	tty = isatty (STDIN_FILENO);
-	if (tty)
+	pam_thinkfinger.isatty = isatty (STDIN_FILENO);
+	if (pam_thinkfinger.isatty == 1)
 		tcgetattr (STDIN_FILENO, &term_attr);
 
 	pam_thinkfinger.retval = PAM_SERVICE_ERR;
@@ -144,9 +191,11 @@ int pam_sm_authenticate (pam_handle_t *pamh, int flags, int argc, const char **a
 	if (pam_thinkfinger_check_user (pam_thinkfinger.user))
 		goto out;
 
-	pam_thinkfinger.tf = libthinkfinger_new ();
-	if (!pam_thinkfinger.tf)
+	pam_thinkfinger.tf = libthinkfinger_new (&init_status);
+	if (init_status != TF_INIT_SUCCESS) {
+		handle_error (pamh, init_status);
 		goto out;
+	}
 
 	pam_thinkfinger.pamh = pamh;
 	pthread_mutex_init(&pam_thinkfinger.retval_mutex, NULL);
@@ -157,7 +206,7 @@ int pam_sm_authenticate (pam_handle_t *pamh, int flags, int argc, const char **a
 
 	if (pam_thinkfinger.tf)
 		libthinkfinger_free (pam_thinkfinger.tf);
-	if (tty) {
+	if (pam_thinkfinger.isatty == 1) {
 		tcsetattr (STDIN_FILENO, TCSADRAIN, &term_attr);
 	}
 
